7ad60d3d5df0bf3865a47bf872bce8cf8e73f886
olivierlabelle <olivier.labelle@metix.ca>
2018-04-16 17:44:51 -0400

    tryin to fix something

--- popUp/DepenseDetails.kt
+++ popUp/DepenseDetails.kt
@@ -1,5 +1,3 @@
-package com.example.android.budgetproject.popUp
-
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentValues
@@ -18,42 +16,29 @@ import java.text.NumberFormat
  */
 object DepenseDetails {
 
+    lateinit var transactionAdapter: TransactionAdapter
+
     @SuppressLint("InflateParams")
     fun createTransaction(context: Activity): AlertDialog {
-        val activity = MainActivity()
         val alertDialog = AlertDialog.Builder(context)
         val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val alertLayout = layoutInflater.inflate(R.layout.new_transaction, null, false) as RelativeLayout
         alertDialog.setView(alertLayout)
         context.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
-        val edMontant = alertLayout.findViewById<EditText>(R.id.ed_montant)
+        val edDepense = alertLayout.findViewById<EditText>(R.id.ed_montant)
         val edDescription = alertLayout.findViewById<EditText>(R.id.ed_description)
         val edDate = alertLayout.findViewById<EditText>(R.id.ed_date)
-        val transaction = ContentValues()
-        val db = TransactionDBHelper(BudgetApplication.getContext()).writableDatabase
-        alertDialog.setPositiveButton(R.string.create_new, DialogInterface.OnClickListener { dialog, which ->
-            val montant = edMontant.text.toString()
+        alertDialog.setPositiveButton(R.string.create_new, { dialog, which ->
+            val depense = edDepense.text.toString()
             val description = edDescription.text.toString()
             val date = edDate.text.toString()
-            transaction.put("Depense", montant)
-            transaction.put("Description", description)
-            transaction.put("Date", date)
-            db.insert("TN_Transaction", null, transaction)
-            activity.notifyDataChanged()
-
+            TransactionCreation().transactionCreation(depense, description, date)
+            transactionAdapter.notifyDataSetChanged()
             //Todo Assurer 2 champs sont valide.
-            //Todo Saver les donners dans la DB
         })
-        alertDialog.setNegativeButton(R.string.cancel, DialogInterface.OnClickListener{ dialog, which ->
+        alertDialog.setNegativeButton(R.string.cancel, { dialog, which ->
         })
         alertDialog.show()
         return alertDialog.create()
     }
-
-   //ContentValues insertValues = new ContentValues();
-   //insertValues.put("Description", "Electricity");
-   //insertValues.put("Amount", 500);
-   //insertValues.put("Trans", 1);
-   //insertValues.put("EntryDate", "04/06/2011");
-   //db.insert("CashData", null, insertValues);
 }
